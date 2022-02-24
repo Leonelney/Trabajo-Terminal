b866f46843e58a6aa650f12d7ff60d65e586b955
leonelOC <leonelolivares80@gmail.com>
2022-02-23 22:00:56 -0600

    extract full data

--- extract.py
+++ extract.py
@@ -5,33 +5,37 @@ import credentials
 
 
 def get_client():
-    client = tweepy.Client(credentials.BEARER_TOKEN)
-    """client = tweepy.Client(consumer_key=credentials.CONSUMER_KEY,
+    client = tweepy.Client(bearer_token=credentials.BEARER_TOKEN,
+                           consumer_key=credentials.CONSUMER_KEY,
                            consumer_secret=credentials.CONSUMER_SECRET,
                            access_token=credentials.ACCESS_TOKEN,
                            access_token_secret=credentials.ACCESS_TOKEN_SECRET
-                           )"""
+                           )
     return client
 
 
 def search_tweets(query):
     client = get_client()
     tweets = client.search_recent_tweets(query=query,
-                                         max_results=10,
-                                         tweet_fields=["created_at", "geo", "lang"]
+                                         max_results=40,
+                                         tweet_fields=["created_at", "geo", "lang"],
+                                         expansions=["geo.place_id"]
                                          )
     tweets_data = tweets.data
+    tweets_includes = tweets.includes
+    if tweets_includes:
+        dict_places = {places.id:places.full_name for places in tweets_includes['places']}  
     results = []
 
     if len(tweets_data) > 0:
         for tweet in tweets_data:
             obj = {}
-            obj['id'] = tweet.id
-            obj['text'] = tweet.text
-            obj['created_at'] = (tweet.created_at - datetime.timedelta(hours=6)).strftime('%Y-%m-%d %H:%M:%S')
-            obj['geo'] = tweet.geo
-            obj['lang'] = tweet.lang
+            obj['data'] = tweet['data']
+            obj['data']['created_at'] = (tweet.created_at - datetime.timedelta(hours=6)).strftime('%Y-%m-%d %H:%M:%S')
+            if tweet.geo:
+                obj['data']['geo_name'] = dict_places[tweet.geo['place_id']]
             results.append(obj)
+
     else:
         print("No tweets found")
         return
