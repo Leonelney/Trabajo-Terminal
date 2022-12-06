884c14cac82c696997cc5ea9305d1608359ce0e1
leonelOC <leonelolivares80@gmail.com>
2022-12-05 19:27:47 -0600

    ultima versión de la extracción de tweets y obtención de los tweets de 2019 a 2022

--- 01_extract.py
+++ 01_extract.py
@@ -82,25 +82,26 @@ def save_register(tweet, query, typeQuery, geoid, coordenadas, name_mun):
 
 def search_tweets_snscrape(query, start_date, end_date):
     geo_municipios = [
-        (2, '19.4853286147,-99.1821069423', '5km', 'Azcapotzalco', 'Azcapotzalco'),
-        (3, '19.3266672536,-99.1503763525', '6km', 'Coyoacán', 'Coyoacán'),
-        (4, '19.3246343001,-99.3107285253', '10km', 'Cuajimalpa de Morelos', '("Cuajimalpa de Morelos" OR Cuajimalpa)'),
-        (5, '19.5040652077,-99.1158642087', '9km', 'Gustavo A. Madero', '("Gustavo A. Madero" OR GAM)'),
-        (6, '19.3969118970,-99.0943297970', '5km', 'Iztacalco', 'Iztacalco'),
-        (7, '19.3491663204,-99.0567989642', '9km', 'Iztapalapa' , 'Iztapalapa'),
-        (8, '19.2689765031,-99.2684129061', '10km', 'La Magdalena Contreras', '("La Magdalena Contreras" OR "Magdalena Contreras" OR MC)'),
-        (9, '19.1394565999,-99.0510954218', '11km', 'Milpa Alta', '("Milpa Alta" OR MilpaAlta)'),
-        (10, '19.3361755620,-99.2468197120', '9km', 'Álvaro Obregón' , '("Álvaro Obregón" OR ÁlvaroObregón)'),
-        (11, '19.2769983772,-99.0028216137', '7km', 'Tláhuac', 'Tláhuac'),
-        (12, '19.1983396763,-99.2062207957', '11km', 'Tlalpan', 'Tlalpan'),
-        (13, '19.2451450458,-99.0903636045', '7km', 'Xochimilco', 'Xochimilco'),
-        (14, '19.3806424162,-99.1611346584', '3km', 'Benito Juárez', '("Benito Juárez" OR BenitoJuárez)'),
-        (15, '19.4313734294,-99.1490557562', '4km', 'Cuauhtémoc', 'Cuauhtémoc'),
-        (16, '19.4280623649,-99.2045669144', '5km', 'Miguel Hidalgo', '("Miguel Hidalgo" OR MiguelHidalgo)'),
-        (17, '19.4304954545,-99.0931057959', '5km', 'Venustiano Carranza', '("Venustiano Carranza" OR VenustianoCarranza OR VC)')
+        (2, '19.4853286147,-99.1821069423', '5km', 'Azcapotzalco', 'azcapotzalco'),
+        (3, '19.3266672536,-99.1503763525', '6km', 'Coyoacán', 'coyoacan'),
+        (4, '19.3246343001,-99.3107285253', '10km', 'Cuajimalpa de Morelos', '("cuajimalpa de morelos" OR cuajimalpa)'),
+        (5, '19.5040652077,-99.1158642087', '9km', 'Gustavo A. Madero', '("gustavo a. madero" OR gam)'),
+        (6, '19.3969118970,-99.0943297970', '5km', 'Iztacalco', 'iztacalco'),
+        (7, '19.3491663204,-99.0567989642', '9km', 'Iztapalapa' , 'iztapalapa'),
+        (8, '19.2689765031,-99.2684129061', '10km', 'La Magdalena Contreras', '"magdalena contreras"'),
+        (9, '19.1394565999,-99.0510954218', '11km', 'Milpa Alta', '"milpa alta"'),
+        (10, '19.3361755620,-99.2468197120', '9km', 'Álvaro Obregón' , '"alvaro obregon"'),
+        (11, '19.2769983772,-99.0028216137', '7km', 'Tláhuac', 'tlahuac'),
+        (12, '19.1983396763,-99.2062207957', '11km', 'Tlalpan', 'tlalpan'),
+        (13, '19.2451450458,-99.0903636045', '7km', 'Xochimilco', 'xochimilco'),
+        (14, '19.3806424162,-99.1611346584', '3km', 'Benito Juárez', '"benito juárez"'),
+        (15, '19.4313734294,-99.1490557562', '4km', 'Cuauhtémoc', 'cuauhtemoc'),
+        (16, '19.4280623649,-99.2045669144', '5km', 'Miguel Hidalgo', '"miguel hidalgo"'),
+        (17, '19.4304954545,-99.0931057959', '5km', 'Venustiano Carranza', '"venustiano carranza"')
     ]
 
-    meta_search = '-is:retweet'
+    meta_search = '-is:retweet lang:es'
+    delimitadores = '(reportar OR denuncia OR denunciar OR contaminación OR ilegal OR ruido OR estrés OR contingencia OR precaución OR prohibido OR detengan OR alto)'
     start_date_time = datetime.datetime.strptime(start_date, '%Y-%m-%d') + \
                     datetime.timedelta(hours=6)
     start_date_time = calendar.timegm(start_date_time.utctimetuple())
@@ -110,10 +111,19 @@ def search_tweets_snscrape(query, start_date, end_date):
 
     results = []
     for geoid, coordenadas, radio, name_mun, mun_query in geo_municipios:
-        new_query = f'{query[0]} {query[1]} geocode:{coordenadas},{radio} since:{start_date_time} until:{end_date_time} {meta_search}'
+        if query == "(pirotecnia OR cohete OR 'fuegos artificiales')":
+            new_query = f'{query} geocode:{coordenadas},{radio} since:{start_date_time} until:{end_date_time} {meta_search}'
+        else:
+            new_query = f'{query} {delimitadores} geocode:{coordenadas},{radio} since:{start_date_time} until:{end_date_time} {meta_search}'
+            
         for tweet in list(sntwitter.TwitterSearchScraper(new_query).get_items()):
             results.append(save_register(tweet, query, 'coordenadas', geoid, coordenadas, name_mun))
-        new_query = f'{query[0]} {query[1]} alcaldia {mun_query} since:{start_date_time} until:{end_date_time} {meta_search}'
+        
+        if query == "(pirotecnia OR cohete OR 'fuegos artificiales')":
+            new_query = f'{query} {mun_query} since:{start_date_time} until:{end_date_time} {meta_search}'
+        else:
+            new_query = f'{query} {delimitadores} {mun_query} since:{start_date_time} until:{end_date_time} {meta_search}'
+            
         for tweet in list(sntwitter.TwitterSearchScraper(new_query).get_items()):
             results.append(save_register(tweet, query, 'palabras clave', geoid, coordenadas, name_mun))
 
@@ -121,9 +131,9 @@ def search_tweets_snscrape(query, start_date, end_date):
 
 def main():
     topics = [
-        ("(pirotecnia OR cohete OR 'fuegos artificiales' OR petardo)", ""),
-        ("(tránsito OR tráfico OR circulación OR translado)", ""),
-        ("(incendio OR quema OR humo OR fuego)", "")
+        "(pirotecnia OR cohete OR 'fuegos artificiales')", 
+        "(tránsito OR tráfico)",
+        "(incendio OR humo OR fuego)"
     ]
 
     year = input('Define the year (YYYY): ')
@@ -134,6 +144,7 @@ def main():
             start_date = f'{year}-0{count}-01'
         else:
             start_date = f'{year}-{count}-01'
+        
         if count+1 < 10:
             end_date= f'{year}-0{count+1}-01'
         elif count == 12:
