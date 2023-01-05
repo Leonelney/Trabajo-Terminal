a168466912c010c36819e00332b920eac936e6ec
leonelOC <leonelolivares80@gmail.com>
2023-01-04 23:26:21 -0600

    reestructuración de las tablas de la base de datos

--- 03_load.py
+++ 03_load.py
@@ -18,32 +18,30 @@ def main():
             df = pd.concat([df,df_aux], sort=False, ignore_index=True)
 
     main = df[['pubID', 'topicQuery', 'tweet', 'likeCount', 
-                'replyCount', 'retweetCount', 'authorID', 'geoID']]
+                'replyCount', 'retweetCount', 'authorID']]
     data = df[['pubID', 'tokens', 'mentions', 'hashtags']]
     author = df[['authorID', 'authorName', 'authorUsername',
                 'authorCreatedAt', 'authorVerified', 'followersCount',
                 'followingCount']]
     date = df[['pubDate', 'pubYear', 'pubMonth', 'pubDay', 
                 'pubHour', 'pubMinute']]
-    geo = df[['geoID', 'geoName']]
-    coordinates = df[['longitude', 'latitude']]
+    coordinates = df[['geoID', 'geoName', 'longitude', 'latitude']]
 
     idx = list(df.index)
 
     main['tweet'] = main['tweet'].apply(lambda x: x.replace("\n", " "))
     main['tweet'] = main['tweet'].apply(deEmojify)
     main.insert(7, 'dateID', list(map(lambda x: 'd'+str(x+1), idx)))
+    main.insert(8, 'coordinateID', list(map(lambda x: 'c'+str(x+1), idx)))
     author['authorName'] = author['authorName'].apply(deEmojify)
     author['authorUsername'] = author['authorUsername'].apply(deEmojify)
     date.insert(0, 'dateID', list(map(lambda x: 'd'+str(x+1), idx)))
-    geo.insert(2, 'coordinateID', list(map(lambda x: 'c'+str(x+1), idx)))
     coordinates.insert(0, 'coordinateID', list(map(lambda x: 'c'+str(x+1), idx)))
 
     main.to_csv('./mysql/main.csv', index=False)
     data.to_csv('./mysql/data.csv', index=False)
     author.to_csv('./mysql/author.csv', index=False)
     date.to_csv('./mysql/date.csv', index=False)
-    geo.to_csv('./mysql/geo.csv', index=False)
     coordinates.to_csv('./mysql/coordinates.csv', index=False)
 
 
